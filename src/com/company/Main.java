c26d9a68c562aea889003295e997b7969b849e40
gorja <gorjanhtc@gmail.com>
2022-02-21 09:30:25 +0100

    Domasha3

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 60;
+        int b = 55;
+
+        System.out.println(Math.addExact(a,b));
+    }
+}
